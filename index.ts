600cce279cadf024e5b63d172a782b5a7940ad9b
Paweł Jarecki <paweljarecki10@gmail.com>
2023-10-22 11:05:20 +0200

    Handling actions coming from front end

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json} from "express"
 import cors from "cors"
 import {handleError} from "./utils/errors";
+import {expensesRouter} from "./routers/expense";
 
 const app = express()
 
@@ -9,6 +10,8 @@ app.use(cors({
 }));
 
 app.use(json());
+
+app.use('/expenses', expensesRouter)
 
 app.use(handleError)
 
