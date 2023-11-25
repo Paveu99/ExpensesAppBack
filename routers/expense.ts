1c25aa6afca068ab835891e95004d55c5522bcf8
Paweł Jarecki <paweljarecki10@gmail.com>
2023-11-25 13:39:23 +0100

    New functionality of PUT added to expense.ts and grouping data by category and date also

--- routers/expense.ts
+++ routers/expense.ts
@@ -1,6 +1,6 @@
 import Router from "express"
 import {ExpensesRecord} from "../records/expenses.record";
-import {AddNewExpense} from "../types";
+import {AddNewExpense, ExpenseEntity} from "../types";
 import {ValidationError} from "../utils/errors";
 
 export const expensesRouter =Router()
@@ -8,8 +8,44 @@ export const expensesRouter =Router()
 expensesRouter
     .get('/', async (req, res) => {
         const allExpenses = await ExpensesRecord.listAll()
+        // TODO: POMYŚLEĆ O TYM CZY JAKAŚ INFORMACJA JESZCZE NIE BĘDZIE PRZYDATNA JAK SUMOWANIE
+        const sortedData = allExpenses.sort(
+            (a, b) => new Date(a.month).getTime() - new Date(b.month).getTime()
+        );
 
-        res.json(allExpenses)
+        const expensesGroupedByDate: Record<string, ExpenseEntity[]> = sortedData.reduce(
+            (acc, obj) => {
+                const date = new Date(obj.month);
+                const year = date.getFullYear();
+                const month = date.toLocaleString('en-US', { month: 'long' });
+                const key = `${month} ${year}`;
+
+                acc[key] = acc[key] || [];
+                acc[key].push(obj);
+
+                return acc;
+            },
+            {} as Record<string, ExpenseEntity[]>
+        );
+
+        const expensesGroupedByCategory: Record<string, ExpenseEntity[]> = sortedData.reduce(
+            (acc, obj) => {
+                const category = obj.category;
+                const key = `${category}`;
+
+                acc[key] = acc[key] || [];
+                acc[key].push(obj);
+
+                return acc;
+            },
+            {} as Record<string, ExpenseEntity[]>
+        );
+
+        res.json({
+            allExpenses,
+            expensesGroupedByDate,
+            expensesGroupedByCategory
+        })
     })
 
     .post('/', async (req, res) => {
@@ -17,6 +53,20 @@ expensesRouter
         await newExpense.insert()
 
         res.json(newExpense)
+    })
+
+    .put('/edit/:id', async (req, res) => {
+        const expense = await ExpensesRecord.getOne(req.params.id)
+        if(!expense) {
+            throw new ValidationError('No such expense!')
+        }
+
+        await expense.updateRecord(req.body)
+
+        res.json({
+            answer: `OK`,
+            name: req.body.name,
+        })
     })
 
     .delete('/:id', async (req, res) => {
